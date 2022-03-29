c492293d8e2ad3cb01be3fd9d24e6dfa8249b237
abhisheksena <abhishekiz4u04@gmail.com>
2022-03-29 14:05:49 +0200

    increase number of combination for test on hemera

--- src/main/java/org/matsim/run/batch/DresdenCalibration.java
+++ src/main/java/org/matsim/run/batch/DresdenCalibration.java
@@ -55,7 +55,7 @@ public class DresdenCalibration implements BatchRun<DresdenCalibration.Params> {
 
 		//@Parameter({0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5})
 
-		@Parameter({ 0.8,0.9, 1.0, 1.1, })
+		@Parameter({ 1.0 })
 		double thetaFactor;
 
 	}
