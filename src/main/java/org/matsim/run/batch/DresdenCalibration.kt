180a91ba0015e462a3fda830874b9d934bb4cf2f
abhisheksena <abhishekiz4u04@gmail.com>
2022-06-03 14:29:00 +0200

    reduced Alpha and increase MUTB, OMICRON infectiousness

--- src/main/java/org/matsim/run/batch/DresdenCalibration.kt
+++ src/main/java/org/matsim/run/batch/DresdenCalibration.kt
@@ -129,7 +129,7 @@ class DresdenCalibration : BatchRun<DresdenCalibration.Params?> {
         val infPerDayB117 = hashMapOf<LocalDate, Int>(
                 LocalDate("2020-01-01") to 0,
                 LocalDate("2020-09-07") to 3,
-                LocalDate("2020-11-14") to 0,
+                LocalDate(params.alpha_zero) to 0, //"2020-11-14"
                 LocalDate(params.summer_alpha) to 10, // summer import "2021-03-01"
                 LocalDate("2021-05-21") to 0)
 
@@ -179,7 +179,7 @@ class DresdenCalibration : BatchRun<DresdenCalibration.Params?> {
         val infPerDayOMICRON: MutableMap<LocalDate, Int> = hashMapOf(
                 LocalDate.parse("2020-01-01") to 0,
                 LocalDate.parse("2022-02-14") to 5,
-                LocalDate("2022-03-15") to 0) // 1 person
+                LocalDate("2022-04-15") to 0) // 1 person
 
         episimConfig.setInfections_pers_per_day(VirusStrain.OMICRON, infPerDayOMICRON)
         virusStrainConfigGroup.getOrAddParams(VirusStrain.OMICRON).apply {
@@ -415,13 +415,14 @@ class DresdenCalibration : BatchRun<DresdenCalibration.Params?> {
         @GenerateSeeds(1)
         var seed = 0L
 
-        @StringParameter("2021-02-21","2021-03-01", "2021-03-07", "2021-03-14" )
+        @StringParameter("2021-04-01","2021-03-21", "2021-04-07", "2021-04-14" )
         lateinit var  summer_alpha: String
 
-        @StringParameter("2021-11-01", "2021-11-07","2021-11-14" )
+        @StringParameter("2021-11-21","2021-11-14" )
         lateinit var  MUTB_zero: String
 
-
+        @StringParameter("2020-11-14","2020-11-07" )
+        lateinit var  alpha_zero: String
 
 
         @Parameter(3.5)
